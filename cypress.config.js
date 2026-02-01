9218e47248cee4119ff3283f1464388f7afc3557
Ajeet Personal <ajeetpersonal@Ajeets-MacBook-Air.local>
2026-02-02 00:00:50 +0530

    Added cypress-mochawesome-reporter for reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.js",
     setupNodeEvents(on, config) {
       config.baseUrl = config.env.baseUrl;
+      require("cypress-mochawesome-reporter/plugin")(on);
       return config;
     },
   },
